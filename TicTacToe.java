197cf35ea2de4279ecb9cdc1e5ab3aac6484e613
felipscarvalho <felipecarvalho5520@gmail.com>
2025-06-04 15:53:19 -0300

    feat: TicTacToe constructor

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,13 @@
 class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    private String [][] ticTacToe;
+
+    public TicTacToe(String [][] ticTacToe) {
+        this.ticTacToe = new String[3][3];
+
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                this.ticTacToe[i][j] = ticTacToe[i][j];
+            }
+        }
     }
 }
