645dc86f6a45d794b300f01cc9937c24584f7eed
Eduardo1Monteiro <eduardomonteiro0047@gmail.com>
2025-06-05 12:31:34 -0300

    Player class update

--- Player.java
+++ Player.java
@@ -1,5 +1,73 @@
+import java.util.Scanner;
+
 class Player {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+  protected String symbol;
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public Player(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public static String chooseSymbol() {
+
+    String symbol = null;
+
+    while (symbol == null) {
+      System.out.println("Choose your symbol:");
+      System.out.println("1) X \n 2) O");
+      System.out.println("Enter 1 or 2:");
+      int choice = scanner.nextInt();
+
+      if (choice == 1) {
+        symbol = "X";
+      } else if (choice == 2) {
+        symbol = "O";
+      } else {
+        System.out.println("Please pick a valid option");
+      }
     }
+
+    return symbol;
+  }
+
+  public static int chooseNumberOfPlayers() {
+    int playersCount = 0;
+
+    while (playersCount == 0) {
+      System.out.println("Choose the number of players:");
+      System.out.println("Type 1 to play against the AI");
+      System.out.println("Type 2 to play against another player");
+      playersCount = scanner.nextInt();
+
+      if (playersCount != 1 && playersCount != 2) {
+        System.out.println("Please choose a valid number of players");
+        playersCount = 0;
+      }
+    }
+
+    return playersCount;
+  }
+
+  public static String chooseDifficulty() {
+
+    String difficulty = null;
+
+    while (difficulty == null) {
+      System.out.println("Choose the difficulty of the AI");
+      System.out.println("Easy) 1");
+      System.out.println("Hard) 2");
+      int choice = scanner.nextInt();
+      if (choice == 1) {
+        difficulty = "Easy";
+      } else if (choice == 2) {
+        difficulty = "Hard";
+      } else {
+        System.out.println("Please pick a valid option");
+      }
+    }
+
+    return difficulty;
+  }
+
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -3,57 +3,75 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 class TicTacToe {
-    private String [][] ticTacToe;
-    private HashMap<String, int[]> positionsMap;
 
-    public TicTacToe() {
-        ticTacToe = new String[3][3];
-    
-        initializeBoard();
-    }
+  protected int difficulty;
+  protected int playerCount;
+  protected Player player1;
+  protected Player player2;
+  private String[][] ticTacToe;
+  private HashMap<String, int[]> positionsMap;
 
-    private void initializeBoard() {
-        int count = 1;
+  public TicTacToe() {
+    ticTacToe = new String[3][3];
 
-        for (int i=0; i<3; i++) {
-            for (int j=0; j<3; j++) {
-                ticTacToe[i][j] = Integer.toString(count);
-                count++;
-            }
-        }
+    initializeBoard();
+  }
 
-        initializeHashMap();
-    }
+  private void initializeBoard() {
+    int count = 1;
 
-    private void initializeHashMap() {
-        positionsMap = new HashMap<>();
-        int count = 1;
-
-        for (int i=0; i<3; i++) {
-            for (int j=0; j<3; j++) {
-                positionsMap.put(Integer.toString(count), new int[] {i, j});
-                count++;
-            }
-        }
-    }
-
-    public void printTable () {
-      for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-           if(j != 2) {
-            System.out.printf("%c |" , ticTacToe[i][j]);
-           } 
-           else if(j == 2) {
-             System.out.printf("%c \n", ticTacToe[i][j]);
-           }
-        }
-        System.out.print("--------------------");
-        System.out.print("\n");
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        ticTacToe[i][j] = Integer.toString(count);
+        count++;
       }
     }
 
-    public static void main(String[] args) {
-        TicTacToe tic = new TicTacToe();
+    initializeHashMap();
+  }
 
+  private void initializeHashMap() {
+    positionsMap = new HashMap<>();
+    int count = 1;
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        positionsMap.put(Integer.toString(count), new int[] { i, j });
+        count++;
+      }
     }
+  }
+
+  public void printTable() {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (j != 2) {
+          System.out.printf("%s |", ticTacToe[i][j]);
+        } else if (j == 2) {
+          System.out.printf("%s \n", ticTacToe[i][j]);
+        }
+      }
+      System.out.print("--------------------");
+      System.out.print("\n");
+    }
+  }
+
+  public void setupPlayers() {
+
+    playerCount = Player.chooseNumberOfPlayers();
+
+    player1 = new Player(Player.chooseSymbol());
+
+    String symbol2 = player1.symbol.equals("X") ? "O" : "X";
+
+    player2 = new Player(symbol2);
+
+    System.out.println("Player 1 is the symbol " + player1.symbol);
+    System.out.println("Player 2 is the symbol " + player2.symbol);
+  }
+
+  public static void main(String[] args) {
+    TicTacToe tic = new TicTacToe();
+
+  }
 }
